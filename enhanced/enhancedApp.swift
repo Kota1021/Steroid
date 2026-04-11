73596a3b7f43d5690fbf51ee318bc931d168d7ab
Kota1021 <kota.matsumoto11@gmail.com>
2026-04-11 18:16:00 +0900

    Initial Commit

---
+++ enhanced/enhancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  enhancedApp.swift
+//  enhanced
+//
+//  Created by KotaroMatsumoto on 2026/04/11.
+//
+
+import SwiftUI
+
+@main
+struct enhancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
